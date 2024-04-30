93e809a58643ce7bb907500553eabf3b28709379
a1 <shitlaoge@gmail.com>
2024-04-30 09:44:33 +0400

    context的使用

--- lib/testkey/TestSetstate.dart
+++ lib/testkey/TestSetstate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 main() {
   runApp(const MyApp());
@@ -34,21 +35,72 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print('context= $context');  ///  MyHomePage
+    var renderBox = ((context as Element).findRenderObject() as RenderBox).size;
+    print('渲染对象：${renderBox}'); //MyHomePage的大小
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text('$count', style: const TextStyle(fontSize: 30.0)),
+        child: Column(
+          children: [
+            ///第一种
+            Foo(count),
+            ///第二种
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                print('context=$context'); ///LayoutBuilder   LayoutBuilder的上一级才有Scaffold的
+                return ElevatedButton(
+                  onPressed: () {Scaffold.of(context).openDrawer();},
+                  child: const Text("打开抽屉"),
+                );
+              },
+            ),
+            /// 我们不需要 BoxConstraints所以可以不要Layout
+            Builder(builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () {Scaffold.of(context).openDrawer();},
+                child: const Text("打开抽屉"),
+              );
+            })
+          ],
+        ),
       ),
+      drawer: const Drawer(),
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           onPressed: () {
-            count++;  //count不放在setState里面，也会刷新
+            count++; //count不放在setState里面，也会刷新
             // setState(() { //setState是一个新设计。  设计出来context就是为了不让你直接使用markNeedsBuild()
             // });
             (context as Element).markNeedsBuild(); //即使没有任何改动，调了这个方法，也会触发build方法
+
+            (context as Element).findAncestorStateOfType(); //找到离我最近的一个state
+            (context as Element).findAncestorWidgetOfExactType(); //找到离我最近的一个widget
+            ///下面这些都是需要用到context
+            Theme.of(context).textTheme;
+            // Provider.of(context);
+            // context.watch();
+            // context.read();
           }),
     );
+  }
+}
+
+class Foo extends StatelessWidget {
+  int count;
+
+  Foo(this.count, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print('==context== $context');
+    return GestureDetector(
+        onTap: () {
+          Scaffold.of(context).openDrawer();
+          print('==onTap==');
+        },
+        child: Text('$count', style: const TextStyle(fontSize: 30.0)));
   }
 }
---
+++ lib/widget效果/Ink_InkWell_Material.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<StatefulWidget> createState() => MyState();
+}
+
+class MyState extends State<MyHomePage> {
+  bool _big = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                print('点击按钮');
+              },
+              style: ButtonStyle(
+                ///按钮颜色
+                backgroundColor: const MaterialStatePropertyAll<Color>(Colors.red),
+
+                ///按钮字体颜色
+                foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      // return Theme.of(context).colorScheme.error.withOpacity(0.5);
+                      return Colors.yellow;
+                    }
+                    return null; // Use the component's default.
+                  },
+                ),
+
+                ///按下的颜色
+                overlayColor: const MaterialStatePropertyAll<Color>(Colors.green),
+              ),
+              child: const Text(
+                'Go to Second Page',
+              ),
+            ),
+            AnimatedContainer(
+              duration: const Duration(microseconds: 500),
+              width: _big ? 200 : 60,
+              height: 60,
+              child: Material(
+                color: Colors.purple,
+                elevation: 4, //影子
+                shape: const StadiumBorder(),
+                child: InkWell(//InkWell的作用就是可能找到上级Material实现水波效果
+                  customBorder: const StadiumBorder(),//水波纹的时候全圆角不要绘制出来
+                  onTap: () => setState(() => _big = !_big),
+                  child: Center(
+                    child: _big ? const Text("Ready!") : const Icon(Icons.arrow_forward_rounded),
+                  ),
+                ),
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
